9d16acf50f9ad0ed2c6d7c98c9c553f63ce309bb
path1heaven <parth1hell@gmail.com>
2020-11-26 13:17:44 +0545

    initial food menu list mock up

--- src/app/feature/secured/order/order.component.ts
+++ src/app/feature/secured/order/order.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { BreakPoints, Grid, GridItem } from '@prajna10/sd_design';
 
 @Component({
   selector: 'app-order',
@@ -7,9 +8,56 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrderComponent implements OnInit {
 
+  grid: BreakPoints<Grid>;
+  searchBar: BreakPoints<GridItem>;
+  category: BreakPoints<GridItem>;
+  foodMenu: BreakPoints<GridItem>;
+
   constructor() { }
 
   ngOnInit(): void {
+    this.initGrid();
+    this.initGridItems();
   }
+
+  initGrid(): void {
+    this.grid = {
+      default: {
+        rows: '5rem 1fr',
+        columns: '[category-start] 25rem [category-end full-start] repeat(10, [col-start] 1fr [col-end])  25rem [full-end]',
+        gap: '1rem'
+      }
+    };
+  }
+
+  initGridItems(): void {
+    this.initSearchBar();
+    this.initCategory();
+    this.initFoodMenu();
+  }
+
+  initSearchBar(): void {
+    this.searchBar = {
+      default: {
+        column: 'col-start 2 / col-end 11'
+      }
+    };
+  }
+
+  initCategory(): void {
+    this.category = {
+      default: {
+        column: 'category-start / category-end'
+      }
+    };
+   }
+
+  initFoodMenu(): void {
+    this.foodMenu = {
+      default: {
+        column: 'full-start / full-end'
+      }
+    }
+   }
 
 }
--- src/app/feature/secured/order/order.module.ts
+++ src/app/feature/secured/order/order.module.ts
@@ -3,12 +3,19 @@ import { CommonModule } from '@angular/common';
 
 import { OrderRoutingModule } from './order-routing.module';
 import { OrderComponent } from './order.component';
+import { FoodMenuComponent } from './food-menu/food-menu.component';
+import { GridModule, ListItemsModule, SdTextInputModule } from '@prajna10/sd_design';
+import { FoodCategoryComponent } from './food-category/food-category.component';
+import { FoodSearchComponent } from './food-search/food-search.component';
 
 
 @NgModule({
-  declarations: [OrderComponent],
+  declarations: [OrderComponent, FoodMenuComponent, FoodCategoryComponent, FoodSearchComponent],
   imports: [
     CommonModule,
+    GridModule,
+    ListItemsModule,
+    SdTextInputModule,
     OrderRoutingModule
   ]
 })
